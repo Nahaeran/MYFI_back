9db8e9cf74e614230f4a89bff3b4af1ed2841b2f
unknown <ctxpfj@naver.com>
2023-11-17 00:28:58 +0900

    articles Model 수정

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from dj_rest_auth.models import AbstractAppUser
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
-    user = models.ForeignKey(AbstractAppUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,7 +11,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(AbstractAppUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
