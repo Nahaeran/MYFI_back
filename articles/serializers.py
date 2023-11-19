9dfea493e28dd6346b7daacdabaddec19e7bc79a
nahaeran <nh2545@naver.com>
2023-11-19 12:44:56 +0900

    feat: post user 정보와 같이 serializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -17,21 +17,14 @@ class PostListSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_fields = ('user',)
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
         read_only_fields = ('post', 'user',)
-
-
-
-
-
-
-
-
