80ea353efb4a9d05b983c8ac1bbd905392808b60
nahaeran <nh2545@naver.com>
2023-11-19 17:11:02 +0900

    fix: 한국시간으로 변경, comment user 정보와 함께 serializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -24,7 +24,8 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('post', 'user',)
+        read_only_fields = ('post',)
--- fianl_pjt_back/settings.py
+++ fianl_pjt_back/settings.py
@@ -187,13 +187,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "ko-kr"
 
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
