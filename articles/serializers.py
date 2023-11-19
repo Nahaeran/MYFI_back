6c0b6f487a83a4e6e360bef5c6ab7ac39743db02
nahaeran <nh2545@naver.com>
2023-11-19 11:38:24 +0900

    feat: postlist user 정보와 같이 serializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Post, Comment
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'name', 'profile_img')
+
+
 class PostListSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content')
+        fields = ('id', 'title', 'user')
 
 
 class PostSerializer(serializers.ModelSerializer):
