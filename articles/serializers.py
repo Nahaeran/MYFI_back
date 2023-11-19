afcf004632d6e53bcf6829c96223556e9fe04c95
nahaeran <nh2545@naver.com>
2023-11-19 11:11:27 +0900

    fix: post, comment 권한 및 응답 수정

--- articles/serializers.py
+++ articles/serializers.py
@@ -18,7 +18,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('post',)
+        read_only_fields = ('post', 'user',)
 
 
 
